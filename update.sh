07e6622a9d7dbbcfed7324b0e5e8f4cd9cb26888
Koichi Murase <myoga.murase@gmail.com>
2016-05-21 03:44:29 +0900

    update pages 20160521

--- update.sh
+++ update.sh
@@ -4,15 +4,18 @@ function dispatch.original-web {
   wget -r http://www.moon.sannet.ne.jp/okahisa/stockham/stockham.html
 
   stockham=www.moon.sannet.ne.jp/okahisa/stockham
+
   (
     cd "$stockham"
     for f in *.html; do
-      iconv -c -f EUC-JP -t UTF-8 "$f" \
+      # 2016-05-21 EUC-JP -> UTF-8 に変わった様だ。
+      cat "$f" \
         | sed -r 's/\bcharset=EUC-JP\b/charset=utf-8/ ; /^(<IMG SRC="http:\/\/www\.sannet\.ne\.jp\/counter\/c[[:digit:]]\.gif" ALT="[[:digit:]]">)+?$/d' > "$f.part" \
         && touch -r "$f" "$f.part" \
         && mv -f "$f.part" "$f"
     done
   )
+
   mv -f "$stockham"/* html/
   rm -rf www.moon.sannet.ne.jp
 }
